5f649a83e282e045e524881f6b0eb3cdeacf440f
Prasamsha Ghimire <159250315+prasamsha-gh@users.noreply.github.com>
2025-04-12 16:17:01 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Function to toggle between light and dark mode
 function toggleTheme() {
     document.body.classList.toggle('dark-mode');
     
-    // Update the button text based on current mode
     const themeToggle = document.getElementById('theme-toggle');
     if (document.body.classList.contains('dark-mode')) {
         themeToggle.innerHTML = '☀️ Light Mode';
